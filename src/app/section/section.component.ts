41e67d2199759d94fb385b153b6d67072e92b3ea
Dane Rossenrode <droces@gmail.com>
2019-05-28 23:34:46 +0200

    Moved section logic and presentation to section component

--- src/app/section/section.component.ts
+++ src/app/section/section.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+
+import { Section } from '../section';
 
 @Component({
   selector: 'app-section',
@@ -6,10 +8,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./section.component.scss']
 })
 export class SectionComponent implements OnInit {
+  @Input() section: Section;
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  addGroup(): void {
+    // section.
+  }
+
+  removeLastGroup(): void {}
 }
