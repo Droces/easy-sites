9e3c6c4c6c8ca4be5039335159e2be0bc864f6b5
Dane Rossenrode <droces@gmail.com>
2019-10-13 05:38:36 +0200

    Styling improvements

--- src/app/structureComponents/page.ts
+++ src/app/structureComponents/page.ts
@@ -1,9 +1,14 @@
 import { Section } from './section';
 
+interface PageState {
+  [key: string]: any
+}
+
 export class Page {
   id: string; // ID of the page in the backend
   path: string;
   navWeight: number;
   title: string;
   sections: Section[];
+  state: PageState;
 }
